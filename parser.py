614fb8a6eea7b224287c49a600e0d9d14c8b63d2
Andreas Wallner <A.Wallner@innovative-solutions.at>
2014-02-17 02:24:53 +0100

    add parser unit test and do some fixes

--- parser.py
+++ parser.py
@@ -5,6 +5,7 @@ from utils import *
 from lxml import etree
 
 class State:
+    class Waiting: pass
     class Meta: pass
     class Ingredients: pass
     class Steps: pass
@@ -12,32 +13,41 @@ class State:
     class FinishRecipe: pass
 
 def parseIngredient(line):
-    r = r'#\s*([0-9/]+(?:\s[0-9/]+)*)\s*([a-zA-Z]+)\s+(.*)'
-    result = re.match( r, line)
-
-    if result is not None:
-        return Ingredient( result.groups()[2], result.groups()[0], result.groups()[1])
-
-    return Ingredient( line.lstrip('# \t'), None, None)
-
-def parseMeta(line,recipe):
-    r = r'!\s*([a-zA-Z]+)\s*:\s*(.*)'
+    r = r'^\s*#\s*([0-9/]+(?:\s[0-9/]+)*)\s*([a-zA-Z]+)\s+(.*)'
     result = re.match( r, line)
 
     if result is None:
-        raise RecipeParseError('invalid meta data line', line)
+        line = line.lstrip('# \t')
+        line = line.rstrip(' \t\n')
+        return Ingredient( line, None, None)
 
-    mn = result.groups()[0]
-    if mn == 'title':
-        recipe.title = result.groups()[1]
-    elif mn == 'size':
-        recipe.size = result.groups()[1]
-    elif mn == 'source':
-        recipe.size = result.groups()[1]
-    elif mn == 'author':
-        recipe.size = result.groups()[1]
+    return Ingredient(
+        result.groups()[2].rstrip(),
+        result.groups()[0],
+        result.groups()[1],
+        )
+
+
+def parseMeta(line,recipe):
+    r = r'^\s*!\s*([a-zA-Z]+)\s*:\s*(.*)'
+    result = re.match( r, line)
+
+    if result is None:
+        raise Exception('invalid metadata line')
+
+    key = result.groups()[0]
+    val = result.groups()[1].rstrip(' \t')
+
+    if key == 'title':
+        recipe.title = val
+    elif key == 'size':
+        recipe.size = val
+    elif key == 'source':
+        recipe.source = val
+    elif key == 'author':
+        recipe.author = val
     else:
-        raise Exception('invalid meta data key', line)
+        raise Exception('invalid metadata key')
 
 def parseFile(stream):
     recipes = []
@@ -45,63 +55,90 @@ def parseFile(stream):
     phase = Phase()
     recipe = Recipe()
 
-    state = State.Meta
+    state = State.Waiting
 
     line = ''
+    line_nr = 0
 
-    while True:
-        if len(line) > 0:
-            start = line[0]
-        else:
-            start = ''
+    try:
+        while True:
+            line = line.lstrip(' \t')
+            if len(line) > 0:
+                start = line[0]
+            else:
+                start = ''
 
-        if state == State.Meta:
-            if start == '!':
-                parseMeta( line, recipe)
-            if start == '#' or start == '*':
-                state = State.Ingredients
-                continue
-
-        elif state == State.Ingredients:
-            if start == '!':
-                state = State.FinishRecipe
-                recipe.phases.append(phase)
-                phase = Phase()
-                continue
-
-            if start == '*':
-                state = State.Steps
-                continue
-
-            if start == '#':
-                phase.ingredients.append(parseIngredient(line))
-
-        elif state == State.Steps:
-            if start == '!' or start == '#':
+            if state == State.Waiting:
                 if start == '!':
-                    state = State.FinishRecipe
-                else:
+                    state = State.Meta
+                    continue
+                
+                if start == '#':
                     state = State.Ingredients
+                    continue
+
+                if start == '*':
+                    state = State.Steps
+                    continue
+
+            if state == State.Meta:
+                if not line:
+                    state = State.FinishRecipe
+                    continue
+
+                if start == '#' or start == '*':
+                    state = State.Ingredients
+                    continue
+
+                if start == '!':
+                    parseMeta( line, recipe)
+
+
+            elif state == State.Ingredients:
+                if start == '!' or not line:
+                    state = State.FinishPhase
+                    continue
+
+                if start == '*':
+                    state = State.Steps
+                    continue
+
+                if start == '#':
+                    phase.ingredients.append(parseIngredient(line))
+
+            elif state == State.Steps:
+                if start == '!' or start == '#' or not line:
+                    state = State.FinishPhase
+                    continue
+
+                if start == '*':
+                    line = line.lstrip('* \t')
+                    line = line.rstrip(' \t\n')
+                    phase.steps.append(Step(line))
+           
+            elif state == State.FinishPhase:
                 recipe.phases.append(phase)
                 phase = Phase()
+                if start == '!' or not line:
+                    state = State.FinishRecipe
+                elif start == '#':
+                    state = State.Ingredients
                 continue
 
-            if start == '*':
-                phase.steps.append(Step(line.lstrip('* \t')))
-        
-
-        elif state == State.FinishRecipe:
-            recipes.append(recipe)
-            recipe = Recipe()
-            state = State.Meta
-
-        line = stream.readline()
-        if line == '':
-            if state != State.Meta:
-                if state == State.Steps or state == State.Ingredients:
-                    recipe.phases.append(phase)
+            elif state == State.FinishRecipe:
                 recipes.append(recipe)
-            break
-        line = line.strip(' \t\n')
+                recipe = Recipe()
+                state = State.Waiting
+                continue
+
+            line = stream.readline()
+            if not line and state == State.Waiting:
+                break
+            else:
+                line_nr += 1
+
+    except Exception as e:
+        line = line.rstrip('\n')
+        raise RecipeParseError(line, line_nr) from e
 
     return recipes
---
+++ test_parser.py
@@ -0,0 +1,158 @@
+import unittest
+from io import StringIO
+from parser import *
+
+class parseIngredientTest(unittest.TestCase):
+    def test_normal(self):
+        i = parseIngredient('\t # 25 g butter   \n')
+        self.assertEqual(i, Ingredient('butter', '25', 'g'))
+
+    def test_nospaces(self):
+        i = parseIngredient('#25g butter')
+        self.assertEqual(i, Ingredient('butter', '25', 'g'))
+
+    def test_noamount(self):
+        i = parseIngredient('\t #diced onion   \n')
+        self.assertEqual(i, Ingredient('diced onion', None, None))
+
+class parseMetaTest(unittest.TestCase):
+    def test_title(self):
+        r = Recipe()
+        m = parseMeta('\t ! title: my title   \n', r)
+        self.assertEqual(r, Recipe('my title'))
+
+    def test_nospace_title(self):
+        r = Recipe()
+        m = parseMeta('!title:my title', r)
+        self.assertEqual(r, Recipe('my title'))
+    
+    def test_size(self):
+        r = Recipe()
+        m = parseMeta('! size: for 4 people', r)
+        self.assertEqual(r, Recipe(None, 'for 4 people'))
+
+    def test_source(self):
+        r = Recipe()
+        m = parseMeta('! source: internet', r)
+        self.assertEqual(r, Recipe(None, None, 'internet'))
+
+    def test_author(self):
+        r = Recipe()
+        m = parseMeta('! author: myself', r)
+        self.assertEqual(r, Recipe(None, None, None, 'myself'))
+
+    def test_unkown(self):
+        with self.assertRaises(Exception) as context:
+            parseMeta('! unknown: foo', None)
+        self.assertEqual(context.exception.args[0], 'invalid metadata key')
+
+    def test_invalid(self):
+        with self.assertRaises(Exception) as context:
+            parseMeta('! not the excepted', None)
+        self.assertEqual(context.exception.args[0], 'invalid metadata line')
+
+class parseFileTest(unittest.TestCase):
+    def test_simple(self):
+        r = parseFile(StringIO(test_input['simple']))
+        self.assertEqual(r, test_result['simple'])
+
+    def test_multiphase(self):
+        r = parseFile(StringIO(test_input['multiphase']))
+        self.assertEqual(r, test_result['multiphase'])
+
+    def test_multi_recipe(self):
+        r = parseFile(StringIO(test_input['multi_recipe']))
+        self.assertEqual(r, test_result['multi_recipe'])
+
+    def test_meta_error(self):
+        with self.assertRaises(RecipeParseError) as context:
+            parseFile(StringIO(test_input['meta_error']))
+
+        self.assertEqual(context.exception.line, '! unknown: foo')
+        self.assertEqual(context.exception.line_nr, 2)
+        self.assertIsNotNone(context.exception.__cause__)
+        self.assertEqual(context.exception.__cause__.args[0], 'invalid metadata key')
+
+test_input = {
+    'simple' : """
+        ! title: the title
+        # 25 g butter
+        * eat butter   """,
+    'multiphase' : """
+        # 25 g butter
+        * eat butter
+        # 100 g meat
+        * eat meat""",
+    'multi_recipe' : """
+        ! title: rec 1
+        # something
+        ! title: rec 2
+        """,
+    'meta_error' : """
+        ! unknown: foo""",
+    }
+
+test_result = {
+    'simple' : [
+        Recipe(
+            'the title',
+            None,
+            None,
+            None,
+            [
+                Phase(
+                    [
+                        Ingredient('butter', '25', 'g'),
+                        ],
+                    [
+                        Step('eat butter'),
+                        ]
+                    )
+                ]
+            )
+        ],
+    'multiphase' : [
+        Recipe(
+            None,
+            None,
+            None,
+            None,
+            [
+                Phase(
+                    [
+                        Ingredient('butter', '25', 'g'),
+                        ],
+                    [
+                        Step('eat butter')
+                        ]
+                    ),
+                Phase(
+                    [
+                        Ingredient('meat', '100', 'g')
+                        ],
+                    [
+                        Step('eat meat')
+                        ]
+                    )
+                ]
+            )
+        ],
+    'multi_recipe' : [
+        Recipe(
+            'rec 1',
+            None,
+            None,
+            None,
+            [
+                Phase(
+                    [
+                        Ingredient('something', None, None)
+                        ],
+                    )
+                ]
+            ),
+        Recipe(
+            'rec 2'
+            )
+        ],
+    }
