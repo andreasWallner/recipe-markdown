c6d4e0388e5b3dd57774ba9ecdaa8a8946e5310e
Andreas Wallner <A.Wallner@innovative-solutions.at>
2014-02-20 00:39:43 +0100

    cleanup of update hook

--- update-hook.py
+++ update-hook.py
@@ -14,6 +14,12 @@ TARGET='/var/www/localhost/htdocs/uhu01/recipes/'
 XSLT='recipe2html.xslt'
 
 def process( obj_id, target):
+    """ get file from git, process, write to target folder
+
+    Arguments:
+    obj_id -- git object id of the file (as string)
+    target -- target folder path (as string, with trailing slash)
+    """
     stream = io.TextIOWrapper( git.blob_file_handle(obj_id), encoding='utf8')
     r = parseFile(stream)
     rec = serializeRecipes(r)
@@ -21,52 +27,49 @@ def process( obj_id, target):
     et = etree.ElementTree(rec)
     et.write(target,xml_declaration=True,pretty_print=True,encoding='UTF-8')
 
-def cleanup_filename(name):
-    parts = name.split('.')
-    if parts[-1] == 'rmd':
-        return '.'.join(parts[:-1])
-    else:
-        return name
-
 def xml_filename(name):
-    return TARGET + cleanup_filename(name) + '.xml'
+    clean = name.rstrip('.rmd')
+    return TARGET + clean + '.xml'
 
-(ref,old,new) = sys.argv[1:4]
+def main():
+    (ref,old,new) = sys.argv[1:4]
 
-print('starting processing of commit')
+    print('starting processing of commit')
 
-cf = git.changed_files(old,new)
+    cf = git.changed_files(old,new)
 
-# do a dry run to catch errors
-for f in cf:
-    action = f[4]
-    file = ' '.join(f[5:])
-    obj_id = f[3]
-    if action == 'M' or action == 'A' or action == 'C':
-        r = process(obj_id,'/dev/null')
+    # do a dry run to catch errors
+    for f in cf:
+        action = f[4]
+        file = ' '.join(f[5:])
+        obj_id = f[3]
+        if action == 'M' or action == 'A' or action == 'C':
+            r = process(obj_id,'/dev/null')
 
-# do a real run
-for c in cf:
-    action = c[4]
-    file = ' '.join(c[5:])
-    obj_id = c[3]
-    if action == 'D':
-        print('D {}'.format(file))
-        try:
-            os.remove(xml_filename(file))
-        except FileNotFoundError:
-            print('file to be removed, but could not be found'.format(file))
+    # do a real run
+    for c in cf:
+        action = c[4]
+        file = ' '.join(c[5:])
+        obj_id = c[3]
+        if action == 'D':
+            print('D {}'.format(file))
+            try:
+                os.remove(xml_filename(file))
+            except FileNotFoundError:
+                print('file to be removed, but could not be found'.format(file))
 
-    elif action == 'M' or action == 'A' or action == 'C':
-        print('C {}'.format(file))
-        process(obj_id, xml_filename(file))
+        elif action == 'M' or action == 'A' or action == 'C':
+            print('C {}'.format(file))
+            process(obj_id, xml_filename(file))
 
-    else:
-        print('unknown git status {} of <{}>'.format(action,file), file=sys.stderr)
+        else:
+            print('unknown git status {} of <{}>'.format(action,file), file=sys.stderr)
 
-index.update_index(TARGET)
+    index.update_index(TARGET)
 
-print('finished processing of commits')
+    print('finished processing of commits')
 
-exit(1)
+    exit(1)
 
+if __name__ == '__main__':
+    main()
