399382411c841ee33ad7288deb434821d36e34b9
Andreas Wallner <A.Wallner@innovative-solutions.at>
2014-07-09 02:57:41 +0200

    fixed special character problems in git interface

--- git.py
+++ git.py
@@ -2,6 +2,32 @@ import os
 import io
 import subprocess
 
+def ls_tree(treeish):
+    # return all file names and object ids of the given treeish
+    git_call = ['git', 'ls-tree', '-r', treeish]
+
+    environ = os.environ.copy()
+    proc = subprocess.Popen(
+        git_call,
+        stdout = subprocess.PIPE,
+        env = environ,
+        )
+    output = proc.communicate()
+
+    if proc.returncode != 0:
+        print('git process failed ({})'.format(proc.returncode))
+        exit(1)
+
+    result = []
+    for l in output[0].decode('utf8').rstrip('\n').split('\n'):
+        s = l.rstrip('\n').split()
+        name = _unscramble_name(s[3:])
+
+        line = s[:3] + [name]
+        result.append(line)
+
+    return result
+
 def changed_files(old,new):
     # if old is before the first commit, it will be all zeros, which is invalid
     # therefore just make it blank
@@ -21,7 +47,17 @@ def changed_files(old,new):
         print('git process failed ({})'.format(proc.returncode))
         exit(1)
 
-    return [ l.rstrip('\n').split() for l in output[0].decode('utf8').rstrip('\n').split('\n') ]
+    # elements [5:] will contain the name after the split,
+    # we have to piece that stuff back together
+    result = []
+    for l in output[0].decode('utf8').rstrip('\n').split('\n'):
+        s = l.rstrip('\n').split()
+        name = _unscramble_name(s[5:])
+
+        line = s[:5] + [name]
+        result.append(line)
+
+    return result
 
 def blob_file_handle(obj_id):
     environ = os.environ.copy()
@@ -32,3 +68,16 @@ def blob_file_handle(obj_id):
         )
     proc.wait(1000)
     return proc.stdout
+
+import codecs
+def _unscramble_name(nameparts):
+    name = ' '.join(nameparts)
+    # no special chars
+    if '\\' not in name:
+        return name
+
+    if name[0] == '"':
+        name = name.strip('"')
+
+    return codecs.escape_decode(name)[0].decode()
+
--- test_git.py
+++ test_git.py
@@ -35,9 +35,21 @@ class tests(unittest.TestCase, utils.TypeCheckMixin):
     def _second_commit(self):
         _write_file('new', 'bar')
         _write_file('change', 'foo')
+        _write_file('with space', 'asdf')
+        _write_file('utf8_ß', 'damn utf8')
+        _write_file('"quote', 'content')
         _run_command(['git', 'rm', 'delete'])
         _run_command(['git', 'add', '.'])
         _run_command(['git', 'commit', '-m second'])
+
+    def test_ls_tree(self):
+        with tempfile.TemporaryDirectory() as tmp:
+            with utils.ChangeDir(tmp):
+                self._setup_git()
+                self._second_commit()
+
+                tree = git.ls_tree('HEAD')
+                self.assertEqual(tree, result['tree'])
 
     def test_changed_files(self):
         with tempfile.TemporaryDirectory() as tmp:
@@ -60,14 +72,24 @@ class tests(unittest.TestCase, utils.TypeCheckMixin):
                     self.assertEqual(content, [b'bar'])
 
 result = {
+    'tree' : [
+        ['100644', 'blob', '6b584e8ece562ebffc15d38808cd6b98fc3d97ea', '"quote'],
+        ['100644', 'blob', '19102815663d23f8b75a47e7a01965dcdc96468c', 'change'],
+        ['100644', 'blob', 'ba0e162e1c47469e3fe4b393a8bf8c569f302116', 'new'],
+        ['100644', 'blob', 'b1af804585a1702ac00b132467d87d56e855324d', 'utf8_ß'],
+        ['100644', 'blob', '5e40c0877058c504203932e5136051cf3cd3519b', 'with space'],
+        ],
     'first' : [
         [':000000', '100644', '0000000000000000000000000000000000000000', 'ba0e162e1c47469e3fe4b393a8bf8c569f302116', 'A', 'change'],
         [':000000', '100644', '0000000000000000000000000000000000000000', 'ba0e162e1c47469e3fe4b393a8bf8c569f302116', 'A', 'delete'],
         ],
     'second' : [
+        [':000000', '100644', '0000000000000000000000000000000000000000', '6b584e8ece562ebffc15d38808cd6b98fc3d97ea', 'A', '"quote'],
         [':100644', '100644', 'ba0e162e1c47469e3fe4b393a8bf8c569f302116', '19102815663d23f8b75a47e7a01965dcdc96468c', 'M', 'change'],
         [':100644', '000000', 'ba0e162e1c47469e3fe4b393a8bf8c569f302116', '0000000000000000000000000000000000000000', 'D', 'delete'],
         [':000000', '100644', '0000000000000000000000000000000000000000', 'ba0e162e1c47469e3fe4b393a8bf8c569f302116', 'A', 'new'],
+        [':000000', '100644', '0000000000000000000000000000000000000000', 'b1af804585a1702ac00b132467d87d56e855324d', 'A', 'utf8_ß'],
+        [':000000', '100644', '0000000000000000000000000000000000000000', '5e40c0877058c504203932e5136051cf3cd3519b', 'A', 'with space'],
         ],
     }
 
